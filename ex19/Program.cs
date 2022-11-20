be1ee7c18e08b372e8daee7e2fbd2d26574c7c85
Roman Borisov <boriso-rom@yandex.ru>
2022-11-20 13:02:35 +0300

    added 12345 strings

--- ex19/Program.cs
+++ ex19/Program.cs
@@ -1,3 +1,9 @@
-﻿Console.Clear();
-Console.WriteLine("Введите пятизначное число: ");
-int userInput = int.Parse(Console.ReadLine());
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int userInput = int.Parse(Console.ReadLine() ?? "");

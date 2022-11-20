91559939f97fb0700ee5f959a1671c95ec73c609
Roman Borisov <boriso-rom@yandex.ru>
2022-11-20 17:39:35 +0300

    last commit in ex21

--- ex21/Program.cs
+++ ex21/Program.cs
@@ -6,18 +6,22 @@
 Console.Clear();
 
 double X1 = userInput("Введите коодинату X1: ");
-double Y1 = userInput("Введите коодинату X1: ");
-double Z1 = userInput("Введите коодинату X1: ");
-double X2 = userInput("Введите коодинату X1: ");
-double Y2 = userInput("Введите коодинату X1: ");
-double Z2 = userInput("Введите коодинату X1: ");
+double Y1 = userInput("Введите коодинату Y1: ");
+double Z1 = userInput("Введите коодинату Z1: ");
+double X2 = userInput("Введите коодинату X2: ");
+double Y2 = userInput("Введите коодинату Y2: ");
+double Z2 = userInput("Введите коодинату Z2: ");
 
-double result = DisPoint(X1, Y1, Z1, X2, Y2, Z2);
-Console.WriteLine($"{result}");
+double result = DistancePoints(X1, Y1, Z1, X2, Y2, Z2);
 
-static double DisPoint(
-double X1, double Y1, double Z1, 
-double X2, double Y2, double Z2)
+static double DistancePoints(double X1, double Y1, double Z1, double X2, double Y2, double Z2)
 {
-    return Math.Sqrt()
+    return Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
 }
+
+static double userInput(string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine() ?? ""); 
+}
+Console.Write($"A({X1}, {Y1}, {Z1}) B({X2}, {Y2}, {Z2}) -> {result}");
